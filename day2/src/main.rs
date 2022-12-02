458d6ec627158f82e6bb09d4a17d0ed3373b3066
Samuel James <507.s94j@gmail.com>
2022-12-02 21:27:00 +0000

    Remove vec allocations

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,28 +1,25 @@
-use std::{fs::read_to_string, time::Instant};
+use std::time::Instant;
 
-const FILE_NAME: &str = "input.txt";
 const MY_BASELINE: i32 = 'X' as i32;
 const OPPONENT_BASELINE: i32 = 'A' as i32;
 const SCORES: [i32; 3] = [3, 6, 0];
 const DECISIONS: [i32; 3] = [1, 2, 3];
 
 fn main() {
-    part_1();
-    part_2();
+    let input = include_str!("../input.txt");
+    part_1(input);
+    part_2(input);
 }
 
-fn part_1() {
+fn part_1(input: &str) {
     let start = Instant::now();
-    let score: i32 = get_input()
+    let score: i32 = input
         .lines()
         .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|c| c.chars().next().unwrap())
-                .collect::<Vec<char>>()
-                .try_into()
-                .unwrap()
+            let mut c = line.chars();
+            (c.next().unwrap(), c.nth(1).unwrap())
         })
-        .fold(0, |total, [opponent, me]: [char; 2]| {
+        .fold(0, |total, (opponent, me)| {
             total
                 + DECISIONS[(me as i32 - MY_BASELINE) as usize]
                 + SCORES[(((me as i32 - MY_BASELINE) - (opponent as i32 - OPPONENT_BASELINE))
@@ -31,26 +28,19 @@ fn part_1() {
     println!("Part 1:{score} in {}us", start.elapsed().as_micros())
 }
 
-fn part_2() {
+fn part_2(input: &str) {
     let start = Instant::now();
-    let score: i32 = get_input()
+    let score: i32 = input
         .lines()
         .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|c| c.chars().next().unwrap())
-                .collect::<Vec<char>>()
-                .try_into()
-                .unwrap()
+            let mut c = line.chars();
+            (c.next().unwrap(), c.nth(1).unwrap())
         })
-        .fold(0, |total, [opponent, me]: [char; 2]| {
+        .fold(0, |total, (opponent, me)| {
             total
                 + DECISIONS[((opponent as i32 - OPPONENT_BASELINE) + (me as i32 - MY_BASELINE - 1))
                     .rem_euclid(3) as usize]
                 + SCORES[(me as i32 - MY_BASELINE + 2).rem_euclid(3) as usize]
         });
     println!("Part 2:{score} in {}us", start.elapsed().as_micros())
-}
-
-fn get_input() -> String {
-    read_to_string(FILE_NAME).unwrap()
 }
