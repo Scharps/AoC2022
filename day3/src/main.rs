15df8ed3aef07974f811e66c56193e788b6886f3
Samuel James <507.s94j@gmail.com>
2022-12-03 23:53:59 +0000

    Part 2 done

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,27 +1,63 @@
+use std::time::Instant;
+
 const CAP_START: u8 = b'A';
 const LOWER_START: u8 = b'a';
 
 fn main() {
     let input = include_str!("../input.txt");
     part_1(input);
+    part_2(input);
 }
 
 fn part_1(input: &str) {
+    let start = Instant::now();
     let total = input
         .lines()
         .map(|line| line.split_at(line.len() / 2))
         .fold(0, |total: u32, (a, b)| {
             for c in a.chars() {
                 if b.contains(c) {
-                    let c = c as u8;
-                    return match c {
-                        65..=90 => total + (c - CAP_START + 1) as u32 + 26,
-                        97..=122 => total + (c - LOWER_START + 1) as u32,
-                        _ => panic!(),
-                    };
+                    return total + score_char(c);
                 }
             }
             total
         });
-    println!("{total}")
+    println!("Part 1:{total} in {}us", start.elapsed().as_micros());
+}
+
+fn part_2(input: &str) {
+    let start = Instant::now();
+    let total =
+        input
+            .lines()
+            .enumerate()
+            .fold((0, ["", "", ""]), |(total, mut concat), (index, line)| {
+                let elf_n = index % 3;
+                concat[elf_n] = line;
+                if elf_n == 2 {
+                    if let Some(c) = concat[0]
+                        .chars()
+                        .chain(concat[1].chars())
+                        .chain(concat[2].chars())
+                        .find(|c| {
+                            concat[0].contains(*c)
+                                && concat[1].contains(*c)
+                                && concat[2].contains(*c)
+                        })
+                    {
+                        return (total + score_char(c), concat);
+                    }
+                }
+                (total, concat)
+            });
+    println!("Part 2:{} in {}us", total.0, start.elapsed().as_micros());
+}
+
+fn score_char(c: char) -> u32 {
+    let c = c as u8;
+    match c {
+        65..=90 => (c - CAP_START + 1) as u32 + 26,
+        97..=122 => (c - LOWER_START + 1) as u32,
+        _ => panic!(),
+    }
 }
